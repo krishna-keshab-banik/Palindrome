fd48d2433b461824cabd40792d6d5c829b225502
krishna-keshab-banik <kb8234@srmist.edu.in>
2026-02-17 09:40:24 +0530

    Initialized

---
+++ Palindrome_Checker/src/PalindromeCheckerApp.java
@@ -0,0 +1,10 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+
+        System.out.println("welcome to the Palindrome App");
+        System.out.println(" version: 1.0");
+        System.out.println("System initialized successfully");
+
+    }
+}
+
